c246e2f51ff3d413d2d7450e2ef22c667a5f6e01
Gautham Ramachandran <gautham2r@gmail.com>
2019-03-11 21:05:11 +0530

    color change.

--- colors.js
+++ colors.js
@@ -2,6 +2,6 @@ export const BlockColorOne = '#35334c';
 
 export const BlockColorTwo = '#12497C';
 
-export const BlockColorThree = '#433e65';
+export const BlockColorThree = '#12497C';
 
-export const BlockColorFour = '#7C6BE3';
+export const BlockColorFour = '#35334c';
